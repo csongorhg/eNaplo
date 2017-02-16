f98ab490e86c7f7d08b1ee48370460bab2782351
mordes <csongorhegedus2@indamail.hu>
2017-02-16 06:53:57 +0100

    Előző sql out nem volt jó.

--- SQL_DB/HCS_SQL.sql
+++ SQL_DB/HCS_SQL.sql
@@ -9,6 +9,20 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for enaplo
+CREATE DATABASE IF NOT EXISTS `enaplo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci */;
+USE `enaplo`;
+
+
+-- Dumping structure for tábla enaplo.diak
+CREATE TABLE IF NOT EXISTS `diak` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NEV` varchar(50) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '0',
+  `osztalyID` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
 -- Dumping data for table enaplo.diak: ~4 rows (approximately)
 /*!40000 ALTER TABLE `diak` DISABLE KEYS */;
 INSERT INTO `diak` (`ID`, `NEV`, `osztalyID`) VALUES
@@ -18,7 +32,17 @@ INSERT INTO `diak` (`ID`, `NEV`, `osztalyID`) VALUES
 	(4, 'János Piter', 2);
 /*!40000 ALTER TABLE `diak` ENABLE KEYS */;
 
--- Dumping data for table enaplo.jegy: ~0 rows (approximately)
+
+-- Dumping structure for tábla enaplo.jegy
+CREATE TABLE IF NOT EXISTS `jegy` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `diakID` int(10) unsigned NOT NULL,
+  `tantargyID` int(10) unsigned DEFAULT NULL,
+  `JEGY` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- Dumping data for table enaplo.jegy: ~3 rows (approximately)
 /*!40000 ALTER TABLE `jegy` DISABLE KEYS */;
 INSERT INTO `jegy` (`ID`, `diakID`, `tantargyID`, `JEGY`) VALUES
 	(3, 2, 2, 3),
@@ -26,12 +50,31 @@ INSERT INTO `jegy` (`ID`, `diakID`, `tantargyID`, `JEGY`) VALUES
 	(5, 1, 3, 5);
 /*!40000 ALTER TABLE `jegy` ENABLE KEYS */;
 
+
+-- Dumping structure for tábla enaplo.osztaly
+CREATE TABLE IF NOT EXISTS `osztaly` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `EVFOLYAM` tinyint(4) DEFAULT NULL,
+  `BETU` char(3) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `KEZDES` smallint(5) unsigned DEFAULT NULL,
+  `SZAK` varchar(150) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
 -- Dumping data for table enaplo.osztaly: ~2 rows (approximately)
 /*!40000 ALTER TABLE `osztaly` DISABLE KEYS */;
 INSERT INTO `osztaly` (`ID`, `EVFOLYAM`, `BETU`, `KEZDES`, `SZAK`) VALUES
 	(1, 13, 'A', 1222, 'Informatika'),
 	(2, 13, 'B', 1222, 'Közgazdaság');
 /*!40000 ALTER TABLE `osztaly` ENABLE KEYS */;
+
+
+-- Dumping structure for tábla enaplo.tantargy
+CREATE TABLE IF NOT EXISTS `tantargy` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NEV` varchar(50) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- Dumping data for table enaplo.tantargy: ~3 rows (approximately)
 /*!40000 ALTER TABLE `tantargy` DISABLE KEYS */;
