81bc4a833db159a64332f0555658824740a6ba59
mordes <csongorhegedus2@indamail.hu>
2017-03-19 14:49:10 +0100

    jegyek hónapokra bontva

--- jegyvalaszto.php
+++ jegyvalaszto.php
@@ -22,7 +22,7 @@ disconnect();
 
 function selectYears()
 {
-    global $conn, $tableTantargy, $tableDiak, $tableEvfolyam, $tableTanev, $tableOsztaly, $tableSzak, $tableTantargySzak;
+    global $conn, $tableTantargy, $tableDiak, $tableEvfolyam, $tableTanev, $tableOsztaly, $tableSzak, $tableTantargySzak, $tableJegy;
 
     echo "<div id='jegyek'>";
 
@@ -39,7 +39,7 @@ function selectYears()
 
 
     //Kiválasztjuk a diák osztálya alapján a lehetséges tantárgyakat
-    $sql = "SELECT ".$tableTantargy.".nev
+    $sql = "SELECT ".$tableTantargy.".nev, ".$tableTantargy.".id
             FROM ".$tableDiak."   
             INNER JOIN ".$tableEvfolyam." ON ".$tableDiak.".id = ".$tableEvfolyam.".diakid
             INNER JOIN ".$tableTanev." ON ".$tableEvfolyam.".tanevid = ".$tableTanev.".id
@@ -76,30 +76,41 @@ function selectYears()
             echo "<tr>";
             echo "<td class='month' >$str</td>";
             
-            //IDE JÖNNEK MAJD A JEGYEK//
-            $sql = "SELECT DISTINCT jegy.jegy, MONTH(jegy.datum) AS month, tantargy.nev
-            FROM diak
-            INNER JOIN evfolyam ON diak.id = evfolyam.diakid
-            INNER JOIN tanev ON evfolyam.tanevid = tanev.id
+            for ($x = 9; $x <= 12; $x++) { //hónapok végigjárása 9 azaz szeptembertől kezdve
+                
+            //IDE JÖNNEK A JEGYEK//
+            $sql2 = "SELECT jegy.evfolyamid, jegy.jegy, tantargy.nev, MONTH(jegy.datum) AS months
+            FROM jegy INNER JOIN evfolyam ON jegy.evfolyamid = evfolyam.id
             INNER JOIN osztaly ON evfolyam.osztalyid = osztaly.id
-            INNER JOIN szak ON osztaly.szakid = szak.id
-            INNER JOIN tantargyszak ON szak.id = tantargyszak.szakid
-            INNER JOIN jegy ON evfolyam.id = jegy.evfolyamid
+            INNER JOIN diak ON evfolyam.diakid = diak.id
+            INNER JOIN tanev ON evfolyam.tanevid = tanev.id
             INNER JOIN tantargy ON jegy.tantargyid = tantargy.id
-            WHERE diak.id = 3 AND osztaly.id = 1 AND tanev.id = 1
-            ORDER BY tantargy.nev, honap";
+            WHERE ".$tableDiak.".id = ".$diak." AND ".$tableOsztaly.".id = ".$osztaly." AND ".$tableTanev.".id = ".$tanev." "
+            ."AND ".$tableTantargy.".id = ".$row["id"]." AND MONTH(".$tableJegy.".datum) = ".$x."
+            ORDER BY ".$tableJegy.".datum";
             
-            $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                          
+            $jegyek = ""; //itt lesznek eltárolva a jegyek az adott hónaphoz
+            $result2 = $conn->query($sql2);
+                if ($result2->num_rows > 0) {
+                    while ($row2 = $result2->fetch_assoc()) {
+                          $jegyek .=$row2["jegy"].", ";
                     }
+                }
+                $jegyek = substr($jegyek, 0, -2); //utolsó vessző nem kell
+                echo "<td class='month' >$jegyek</td>";
+                
+                if ($x == 12) {//ha elértük decembert akkor előről azaz 1
+                    $x = 0;
+                }
+                if ($x == 6) {//ha június akkor vége
+                     break; 
                 }
             }
             //
             
+                
             echo "</tr>";
-
+            } 
         }
     }
 
